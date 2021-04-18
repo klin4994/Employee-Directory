aafbba91c9611e1b11bf959b9649bc39acc4d5c5
klin4994 <kevlin129@hotmail.com>
2021-04-18 17:23:20 +1000

    Setting up initial state and call useState method in App,js

--- application/src/App.js
+++ application/src/App.js
@@ -1,5 +1,11 @@
-import React, { useState, useEffect, useMemo} from "react";
+import React, { useState, useEffect} from "react";
 import axios from "axios";
-
 import Table from "./components/Table";
 
+function App() {
+    // data state to store the randomly generated API data
+    const [data, setData] = useState([]);
+      
+}
+  
+  export default App;
