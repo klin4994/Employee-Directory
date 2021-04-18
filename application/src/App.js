11fe289ff51b97e77ef9faa8c2250c4165d4ebc9
klin4994 <kevlin129@hotmail.com>
2021-04-18 17:19:49 +1000

    Set up Table.js component and imported in App.js

--- application/src/App.js
+++ application/src/App.js
@@ -1,3 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo} from "react";
+import axios from "axios";
 
 import Table from "./components/Table";
+
---
+++ application/src/components/Table.js
@@ -0,0 +1,55 @@
+import React, {useState} from "react";
+import { useTable} from "react-table";
+
+function Table({ columns, data }) {
+    
+    // Use the useTable Hook to send the columns and data to build the table
+    const {
+      getTableProps, // table props from react-table
+      getTableBodyProps, // table body props from react-table
+      headerGroups, // headerGroups that includes all headers
+      rows, // rows for the table based on the data passed
+      prepareRow, // Prepare the row before getting the row props
+      setFilter // sets filters for columns
+    } = useTable(
+        {
+            columns,
+            data
+        },
+    );
+    
+
+
+
+
+    return (
+        <div>
+
+            <table {...getTableProps()}>
+            <thead>
+                {headerGroups.map(headerGroup => (
+                <tr {...headerGroup.getHeaderGroupProps()}>
+                    {headerGroup.headers.map(column => (
+                    <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+                    ))}
+                </tr>
+                ))}
+            </thead>
+            <tbody {...getTableBodyProps()}>
+                {rows.map((row, i) => {
+                prepareRow(row);
+                return (
+                    <tr {...row.getRowProps()}>
+                    {row.cells.map(cell => {
+                        return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
+                    })}
+                    </tr>
+                );
+                })}
+            </tbody>
+            </table>
+        </div>
+      );
+}
+
+export default Table;
