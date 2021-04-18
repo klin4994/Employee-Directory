879b2d0db6496e4d87634c2324e04164374b209d
klin4994 <kevlin129@hotmail.com>
2021-04-18 17:27:03 +1000

    Adding useEffect method to handle axios query to the API and set the data state with the API response

--- application/src/App.js
+++ application/src/App.js
@@ -5,7 +5,16 @@ import Table from "./components/Table";
 function App() {
     // data state to store the randomly generated API data
     const [data, setData] = useState([]);
-      
+  
+    // useEffect method to set the state for data once the component is mounted
+    useEffect(() => {
+      (async () => {
+        const result = await axios("https://randomuser.me/api/?results=20");
+        setData(result.data)
+        console.log(result.data)
+      })();
+    }, []);
+    
 }
   
   export default App;
